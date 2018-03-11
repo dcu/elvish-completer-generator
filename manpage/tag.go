abb7d3d10f7fd91e8937a1dd2f0e5c93064fee5e
David A. Cuadrado <krawek@gmail.com>
2018-03-11 09:42:25 -0500

    Improve support for matching sub commands.

--- manpage/tag.go
+++ manpage/tag.go
@@ -1,13 +1,14 @@
 package manpage
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
 
 var (
 	flagRx       = regexp.MustCompile(`\A(--?[\w-]+)`)
-	subCommandRx = regexp.MustCompile(`\A([a-z_-]+)\z`)
+	subCommandRx = regexp.MustCompile(`\A([a-z]+[a-z_-]*)\S*\z`)
 )
 
 // Tag represents a tag in the document
@@ -22,12 +23,26 @@ func (t *Tag) ToOptions() map[string]string {
 	opts := strings.Split(t.Content[0], ",")
 	content := strings.Join(t.Content[1:], " ")
 	for _, opt := range opts {
+		if Debug {
+			fmt.Printf("looking for flags or subcommands: %s\n", opt)
+		}
+
 		if matches := flagRx.FindStringSubmatch(opt); len(matches) > 1 {
 			result[matches[1]] = strings.TrimSpace(content)
-		} else if matches := subCommandRx.FindStringSubmatch(opt); t.Name == "TP" && len(matches) > 1 {
+			if Debug {
+				fmt.Printf("found flag %s\n", matches[1])
+			}
+		} else if matches := subCommandRx.FindStringSubmatch(opt); t.couldBeSubCommand() && len(matches) > 1 {
 			result[matches[1]] = strings.TrimSpace(content)
+			if Debug {
+				fmt.Printf("found sub command %s\n", matches[1])
+			}
 		}
 	}
 
 	return result
+}
+
+func (t *Tag) couldBeSubCommand() bool {
+	return t.Name == "TP" || t.Name == "PP"
 }
